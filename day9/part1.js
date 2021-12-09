f99d2fbd72e61a31aa108b6248d236e81c117cf1
Sven Maes <sven.maes@ap.be>
2021-12-09 12:34:21 +0100

    d9

--- day9/part1.js
+++ day9/part1.js
@@ -2,3 +2,19 @@ const fs = require('fs');
 let input = fs.readFileSync('day9/input.txt', 'utf-8');
 input =  fs.readFileSync('day9/testInput.txt', 'utf-8');
 
+const heightmap = input.split('\n').map(a => a.split('').map(Number));
+
+let riskLevel = 0;
+
+for (let i = 0; i < heightmap.length; i++) {
+    for (let j = 0; j < heightmap[0].length; j++) {
+        let adjacent = [];
+        if (i >= 1) adjacent.push(heightmap[i - 1][j]);
+        if (j >= 1) adjacent.push(heightmap[i][j - 1]);
+        if (i < heightmap.length - 1) adjacent.push(heightmap[i + 1][j]);
+        if (j < heightmap[0].length - 1) adjacent.push(heightmap[i][j + 1]);
+        if (Math.min(...adjacent) > heightmap[i][j]) riskLevel += heightmap[i][j] + 1;
+    }
+}
+
+console.log(riskLevel);
--- day9/part2.js
+++ day9/part2.js
@@ -1,3 +1,51 @@
 const fs = require('fs');
 let input = fs.readFileSync('day9/input.txt', 'utf-8');
 input =  fs.readFileSync('day9/testInput.txt', 'utf-8');
+
+const heightmap = input.split('\n').map(a => a.split('').map(Number));
+
+let lowPoints = [];
+let basinSizes = []
+
+const getadjacent = (point) => {
+    let adjacent = [];
+    const i = point[0];
+    const j = point[1];
+    if (i >= 1) adjacent.push([i - 1, j]);
+    if (j >= 1) adjacent.push([i, j - 1]);
+    if (i < heightmap.length - 1) adjacent.push([i + 1, j]);
+    if (j < heightmap[0].length - 1) adjacent.push([i, j + 1]);
+    return adjacent;
+}
+
+for (let i = 0; i < heightmap.length; i++) {
+    for (let j = 0; j < heightmap[0].length; j++) {
+        const neighbourValues = getadjacent([i, j]).map(nv => heightmap[nv[0]][nv[1]]);
+        if (Math.min(...neighbourValues) > heightmap[i][j])
+            lowPoints.push([i, j]);
+    }
+}
+
+lowPoints.forEach(point => {
+    let basinSize = 0;
+    const visited = [];
+    const toVisit = [point];
+    while (toVisit.length > 0) {
+        const curr = toVisit.pop();
+        if (visited[curr] == true)
+            continue;
+        visited[curr] = true;
+        basinSize++;
+        getadjacent(curr).forEach(adj => {
+
+            if (heightmap[adj[0]][adj[1]] < 9 && visited[adj] == undefined)
+                toVisit.push(adj);
+
+        });
+    }
+    basinSizes.push(basinSize);
+})
+
+const topThreeBasinSizes = basinSizes.sort((a, b) => b - a).slice(0, 3);
+
+console.log(topThreeBasinSizes.reduce((total, curr) => total * curr));
